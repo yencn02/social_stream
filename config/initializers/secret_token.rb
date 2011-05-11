d4d4d824aad79c51bd36f2e3e5e2740dbd3e7db2
Yen Ha-Thi-Bach <yencn02@gmail.com>
2011-05-11 11:20:09 +0700

    Initialize a new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialStream::Application.config.secret_token = 'a6e1d958e9b966ccfa98502289f250a793b44ad6a978c8a13db0b4045baf1146017413f529df80fd7edd05c50396991dacb1e0015337ee89c4e67e7df34d08d1'
